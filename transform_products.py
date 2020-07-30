c2e6f7e9c1213b391b67098a2f7c4d1d148f65a5
Marco Orellana <orellanas.marco@gmail.com>
2020-07-30 02:24:41 -0600

    Adding network user and updated timestamp

--- transform_products.py
+++ transform_products.py
@@ -4,6 +4,8 @@ import json
 import headers
 from decouple import config
 from cmd_args import cmdline_args
+import getpass
+from datetime import datetime
 import logging
 logger = logging.getLogger(__name__)
 
@@ -14,9 +16,15 @@ def main(filepath, gcs_bucket, gcs_object):
         data = json.load(f)
 
     df = pd.DataFrame(data['results'][0]['items'], columns=headers.products)
+    
+    # Emulate network user using operative system user
+    username = getpass.getuser()
+    logging.info(f"Catalog updated by: {username}")
+    df['added_by'] = username
+    df['added_timestamp'] = datetime.now()
 
     logging.debug(f"Showing first 10 records:\n {df.head(10)}")
-    
+
     # Store transformed results into a distributed storage
     df.to_parquet(f"gs://{gcs_bucket}/{gcs_object}", index=False)
 
