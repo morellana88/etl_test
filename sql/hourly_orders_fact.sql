cbc9a59349000510b04812172e8126ac1386e49e
Marco Orellana <orellanas.marco@gmail.com>
2020-07-31 12:47:31 -0600

    Fix a log error when the validation of the hours of the day is made

--- sql/hourly_orders_fact.sql
+++ sql/hourly_orders_fact.sql
@@ -4,9 +4,9 @@ WITH
     order_id,
     user_id,
     order_number,
-    order_dow day_key,
     -- Assuming the hour 24 is the 00:00 next day
-    IF(order_hour_of_day > 23, 0, order_dow + 1) hour_key,
+    IF(order_hour_of_day > 23, order_dow + 1, order_dow) day_key,
+    IF(order_hour_of_day > 23, 0, order_hour_of_day) hour_key,
     product_name,
     product_number
   FROM
@@ -15,7 +15,7 @@ SELECT
   order_id,
   user_id,
   order_number,
-  -- Adjusting the start day after the hour workaround
+  -- Adjusting the start day of the week
   IF(od.day_key > 6, 0, od.day_key) day_key,
   od.hour_key,
   p.product_key,
